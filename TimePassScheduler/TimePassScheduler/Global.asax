8f167e1cf2be91a8d59fc0ba08988eb9aa82c65c
Priyank Sharma <prisharm8@publicisgroupe.net>
2018-12-14 15:59:07 +0530

    added getting the mock data

---
+++ TimePassScheduler/TimePassScheduler/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TimePassScheduler.MvcApplication" Language="C#" %>
---
+++ TimePassScheduler/TimePassScheduler/Models/SchedulerViewModal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TimePassScheduler.Models
+{
+    public class SchedulerViewModal
+    {
+        public List<County> Counties { get; set; }
+    }
+}
